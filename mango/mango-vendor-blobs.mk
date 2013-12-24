a5f251c2b15757dfb9321b309f0c9f7823c1a766
Michael Bestas <mikeioannina@gmail.com>
2013-12-24 18:47:18 +0200

    mogami: rename camera blobs

--- mango/mango-vendor-blobs.mk
+++ mango/mango-vendor-blobs.mk
@@ -47,7 +47,7 @@ PRODUCT_COPY_FILES += \
     vendor/semc/mango/proprietary/lib/libuim.so:system/lib/libuim.so \
     vendor/semc/mango/proprietary/lib/libwms.so:system/lib/libwms.so \
     vendor/semc/mango/proprietary/lib/libwmsts.so:system/lib/libwmsts.so \
-    vendor/semc/mango/proprietary/lib/hw/vendor-camera.semc.so:system/lib/hw/vendor-camera.semc.so \
+    vendor/semc/mango/proprietary/lib/hw/camera.vendor.semc.so:system/lib/hw/camera.vendor.semc.so \
     vendor/semc/mango/proprietary/lib/libcald_client.so:system/lib/libcald_client.so \
     vendor/semc/mango/proprietary/lib/libcald_debugger.so:system/lib/libcald_debugger.so \
     vendor/semc/mango/proprietary/lib/libcald_hal.so:system/lib/libcald_hal.so \
