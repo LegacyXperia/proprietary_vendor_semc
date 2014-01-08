41a7ca5c19e062c8a9d39f3b1ee26c39635e1bd5
Michael Bestas <mikeioannina@gmail.com>
2014-01-08 19:51:00 +0200

    msm7x30: Add vanilla adreno libs
    Change-Id: I7634817bfb1f7f028641e7643f7c78c576d43800

--- msm7x30-common/msm7x30-common-vendor-blobs.mk
+++ msm7x30-common/msm7x30-common-vendor-blobs.mk
@@ -27,6 +27,8 @@ PRODUCT_COPY_FILES += \
     vendor/semc/msm7x30-common/proprietary/lib/libC2D2.so:system/lib/libC2D2.so \
     vendor/semc/msm7x30-common/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/semc/msm7x30-common/proprietary/lib/libgsl.so:system/lib/libgsl.so \
+    vendor/semc/msm7x30-common/proprietary/lib/libllvm-arm.so:system/lib/libllvm-arm.so \
+    vendor/semc/msm7x30-common/proprietary/lib/libOpenCL.so:system/lib/libOpenCL.so \
     vendor/semc/msm7x30-common/proprietary/lib/libOpenVG.so:system/lib/libOpenVG.so \
     vendor/semc/msm7x30-common/proprietary/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
@@ -34,6 +36,7 @@ PRODUCT_COPY_FILES += \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    vendor/semc/msm7x30-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/semc/msm7x30-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
