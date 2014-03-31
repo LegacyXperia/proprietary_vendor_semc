badb39273051ad50711cbce967f959a48aeb7cf1
Michael Bestas <mikeioannina@gmail.com>
2014-03-31 23:56:46 +0300

    msm7x30: Remove not used adreno library & update S3D.
    Update on the vendor side.
    Pulled from d2 (Galaxy S3).
    
    Change-Id: If585a59e152f650e44cb97a193de748bdc3bf263

--- msm7x30-common/msm7x30-common-vendor-blobs.mk
+++ msm7x30-common/msm7x30-common-vendor-blobs.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-    vendor/semc/msm7x30-common/proprietary/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
     vendor/semc/msm7x30-common/proprietary/lib/libDivxDrm.so:system/lib/libDivxDrm.so \
     vendor/semc/msm7x30-common/proprietary/lib/libExtendedExtractor.so:system/lib/libExtendedExtractor.so \
